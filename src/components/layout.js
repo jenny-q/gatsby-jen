dac6a502a4a76f954f3f4cb6b1001d5a4469ee11
JennyQ <jennifercquispe@gmail.com>
2020-08-23 17:47:08 -0400

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Global, css } from '@emotion/core';
 import Helmet from 'react-helmet';
-import Header from './header';
+import Header from './Header';
 import useSiteMetadata from '../hooks/use-sitemetadata';
 
 const Layout = ({ children }) => {
